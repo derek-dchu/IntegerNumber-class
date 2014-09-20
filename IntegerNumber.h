17ded86189286d0dcba41c52f7255ae74b976911
Derek Hu <woodyhu89@gmail.com>
2014-09-20 18:24:15 -0400

    Initiate header file

---
+++ IntegerNumber.h
@@ -0,0 +1,36 @@
+/*
+
+
+
+
+*/
+
+#ifndef INTEGERNUMBER_H_
+#define INTEGERNUMBER_H_
+
+
+
+using namespace std;
+
+class IntegerNumber {
+private:
+	bool sign;
+	vector<int> digits;
+
+public:
+	
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+#endif
